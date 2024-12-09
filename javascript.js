0f202eadd8c20f4c5c71c5c27f6cc8426b4ca7a0
ArfaGhaisani <arfaaaghaisaniii@gmail.com>
2024-12-09 13:22:51 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById('discover-btn').addEventListener('click', function () {
+    alert('Welcome to Caffeine Cove! ayo liat menu kami, menu yang sediakan cocok untuk kamu yang manis');
+});
+document.getElementById('contact-btn').addEventListener('click', function () {
+    alert('Halo manis, terimakasih sudah menghubungi kami! ^O^');
+});
